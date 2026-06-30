b2832c81fdb6bd3129490fa3cc50d202d5b1ecd1
302A-02\cre <a@a>
2026-06-30 12:15:57 +0900

    0630

--- works/work5/User.php
+++ works/work5/User.php
@@ -1,15 +1,17 @@
 <?php
-class User {
+class User
+{
 
     // ↓フィールド============================
     private int $id;
     private string $name;
     private int $age;
-    private $address;
+    private string $address;
     // ↑フィールド============================
 
     // コンストラクタ
-    function __construct(int $id, string $name, int $age, string $address) {
+    function __construct(int $id, string $name, int $age, string $address)
+    {
         $this->id = $id;
         $this->name = $name;
         $this->age = $age;
@@ -17,42 +19,51 @@ class User {
     }
 
     // idを取得
-    function getId(): int {
+    function getId(): int
+    {
         return $this->id;
     }
 
     // 名前を変更
-    function setName(string $name): void {
+    function setName(string $name): void
+    {
         $this->name = $name;
     }
 
     // 名前を取得
-    function getName(): string {
+    function getName(): string
+    {
         return $this->name;
     }
 
     // 年齢を変更(問題2: バリデーションを追加すること)
-    function setAge(int $age): void {
-        if($age < 0 || $age > 150)
+    function setAge(int $age): void
+    {
+        if ($age < 0 || $age > 150)
             echo "不正な年齢です\n";
         return;
-         $this->age = $age;
+        $this->age = $age;
     }
 
     // 年齢を取得
-    function getAge(): int {
+    function getAge(): int
+    {
         return $this->age;
     }
 
-    function setAddress(string $address): void {
+    function setAddress(string $address): void
+    {
         $this->address = $address;
     }
-    function getAddress(): string {
+    function getAddress(): string
+    {
         return $this->address;
     }
 
     // 問題3
-    function getOlder(): void {
-        return $age+1;
+
+    function getOlder(): void
+    {
+        $this->setAge($this->age + 1);
     }
 }
--- works/work5/main.php
+++ works/work5/main.php
@@ -15,15 +15,19 @@ $mainUser->setAge(30);
 showUserStatus($mainUser);
 showUserStatus($subUser);
 
+$mainUser->getOlder();
+showUserStatus($mainUser);
+
 // バリデーションの確認用(問題2ができたら以下のコメントを外して実行してみよう)
 $mainUser->setAge(-5);   // "不正な年齢です" と表示されるはず
 $mainUser->setAge(200);  // "不正な年齢です" と表示されるはず
 showUserStatus($mainUser); // 年齢は30のまま変わらないはず
 
-function showUserStatus(User $user): void {
-    echo "========ユーザー情報========" ."\n";
-    echo "ID: ".$user->getId()."\n";
-    echo "名前: ".$user->getName()."\n";
-    echo "年齢: ".$user->getAge()."\n";
-    echo "住所: ".$user->getaddress()."\n";
+function showUserStatus(User $user): void
+{
+    echo "========ユーザー情報========" . "\n";
+    echo "ID: " . $user->getId() . "\n";
+    echo "名前: " . $user->getName() . "\n";
+    echo "年齢: " . $user->getAge() . "\n";
+    echo "住所: " . $user->getaddress() . "\n";
 }
